e98b174d924750e853991e417c96202b629b8e84
Vinicius Sebold <63889194+vinisebold@users.noreply.github.com>
2025-08-14 15:11:10 +0000

    chore: remove imports desnecessários

--- src/app/modules/inicio/pages/dashboard/dashboard.ts
+++ src/app/modules/inicio/pages/dashboard/dashboard.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { FormularioCliente } from '../../../cadastros/components/formulario-cliente/formulario-cliente';
-import { Botao } from '../../../../shared/components/botao/botao';
-import { FormularioFornecedor } from "../../../cadastros/components/formulario-fornecedor/formulario-fornecedor";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [FormularioCliente, Botao, FormularioFornecedor],
+  imports: [],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
