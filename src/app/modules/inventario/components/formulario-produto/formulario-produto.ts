d9e10966cf176439b4b0a85bd36224623ba417e8
vinicius_pereira153 <vinicius_pereira153@estudante.sesisenai.org.br>
2025-08-18 16:48:03 +0000

    chore(ui): implementa modal de adicionar produto

--- src/app/modules/inventario/components/formulario-produto/formulario-produto.ts
+++ src/app/modules/inventario/components/formulario-produto/formulario-produto.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-formulario-produto',
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './formulario-produto.scss'
 })
 export class FormularioProduto {
+  @Output() fechar = new EventEmitter<void>();
+  @Output() salvo = new EventEmitter<void>();
+  
 
+  onFecharClick(): void {
+    this.fechar.emit();
+  }
 }
